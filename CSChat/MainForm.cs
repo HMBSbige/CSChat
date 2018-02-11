b8cca420038e956225500e3cf82f093bef964b19
HMBSbige <admin@bige0.com>
2018-02-11 20:25:00 +0800

    Initial Project

---
+++ CSChat/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CSChat
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var cm=new ConnectionManager();
+            cm.Port = 2333;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# CSChat
+最终目标：
+* 点对点发送文本、图片、文件
+
+
+# 参考
+https://github.com/Apollo013/CSharp_AsyncTcpSockets
+
+https://github.com/HMBSbige/Network-programming-training
